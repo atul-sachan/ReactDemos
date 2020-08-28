8f94f9e99ab4238b85243eb92710141416c29f11
atul-sachan <34751474+atul-sachan@users.noreply.github.com>
2020-08-28 20:50:53 +0530

    update stream

--- blog-demo/src/actions/index.js
+++ blog-demo/src/actions/index.js
@@ -1,15 +1,18 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  _.chain(getState().posts)
-    .map('userId')
-    .uniq()
-    .forEach(id => dispatch(fetchUser(id)))
-    .value();
+  // _.chain(getState().posts)
+  //   .map('userId')
+  //   .uniq()
+  //   .forEach(id => dispatch(fetchUser(id)))
+  //   .value();
+  const allUserIds = getState().posts.map(post => post.userId);
+  const uniqueUserIds = [...new Set(allUserIds)];
+  uniqueUserIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 
